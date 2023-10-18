60947e618a7011017e4d0a7f3ad3d31c70508436
Aleksei Strizhak <alexei.mifrill.strizhak@gmail.com>
2023-10-18 22:37:10 +0100

    chore: drop redundant options to fix warnings

--- migrate-mongo-config.ts
+++ migrate-mongo-config.ts
@@ -7,8 +7,6 @@ module.exports = {
   mongodb: {
     url: process.env.DB_URI,
     options: {
-      useNewUrlParser: true, // removes a deprecation warning when connecting
-      useUnifiedTopology: true // removes a deprecating warning when connecting
       //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
       //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
     }
