1a7e6bce2738d5c7a6d06fe066cb92f11d7f8b38
Aleksei Strizhak <alexei.mifrill.strizhak@gmail.com>
2023-10-18 21:46:29 +0100

    feat: apply handleTransaction for migration

--- src/migrations/20231018200150-test.ts
+++ src/migrations/20231018200150-test.ts
@@ -1,25 +1,17 @@
 import { Db, MongoClient } from 'mongodb'
 
+import { handleTransaction } from 'db/handleTransaction'
+
 module.exports = {
   async up(db: Db, client: MongoClient): Promise<void | never> {
-    const session = client.startSession()
-    try {
-      await session.withTransaction(async () => {
-        await db.collection('collection').insertOne({ foo: 'bar' })
-      })
-    } finally {
-      await session.endSession()
-    }
+    await handleTransaction(client, async (_session) => {
+      await db.collection('collection').insertOne({ foo: 'bar' })
+    })
   },
 
   async down(db: Db, client: MongoClient): Promise<void | never> {
-    const session = client.startSession()
-    try {
-      await session.withTransaction(async () => {
-        await db.collection('collection').deleteMany()
-      })
-    } finally {
-      await session.endSession()
-    }
+    await handleTransaction(client, async (_session) => {
+      await db.collection('collection').deleteMany()
+    })
   }
 }
